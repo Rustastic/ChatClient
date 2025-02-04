93e14b98518e1fef68d50106ddea55166e4c9531
AndreaDenina <andredeninaf@gmail.com>
2025-02-04 12:22:40 +0100

    cargo fmt

--- src/packet_cache.rs
+++ src/packet_cache.rs
@@ -17,8 +17,6 @@ impl PacketCache {
     }
 
     pub fn insert(&mut self, packet: Packet) {
-        //the packet will be copied here
-
         let session_id = packet.session_id;
 
         if let PacketType::MsgFragment(fragment) = packet.clone().pack_type {
@@ -26,14 +24,11 @@ impl PacketCache {
 
             self.cache.insert((session_id, fragment_index), (packet, 0));
         } else {
-            //log an error: trying to insert a packet that doesnt contain a fragment
             error!("Trying to insert a packet that doesn't contain a fragment");
         }
     }
 
     pub fn get(&mut self, session_id: u64, fragment_index: u64) -> Option<&(Packet, u64)> {
-        // this function should return a reference to the packet and the number of times it has been requested
-        // it should also increment the number of times the packet has been requested
         match self.cache.get(&(session_id, fragment_index)) {
             Some((packet, counter)) => {
                 let counter = counter + 1;
@@ -52,7 +47,6 @@ impl PacketCache {
         }
     }
 
-    // i want to combine the remove and the get i.e i want to remove the packet and return it
     pub fn remove_and_get(
         &mut self,
         session_id: u64,
